a7722b7feb154ba39f97b894056a9ee1c76ced4e
maguai <xianan.lxa@alibaba-inc.com>
2022-08-23 20:26:03 +0800

    update

--- js/xei-node.js
+++ js/xei-node.js
@@ -5,7 +5,7 @@
 const fs = require('fs');
 const {exec} = require('child_process');
 
-export default {
+module.exports = {
 	/**
 	 * 遍历某个文件夹
 	 * @param {*} folderPath 文件夹路径
@@ -25,14 +25,14 @@ export default {
 	/**
 	 * 获取某个文件夹下的所有文件
 	 * @param {*} folderPath 要遍历的文件夹路径
-	 * @param {*} isIgnoreFolder 是否忽略文件夹
+	 * @param {*} isIgnoreFolder 是否忽略文件夹，默认为true
 	 */
 	getFileList(folderPath, isIgnoreFolder) {
 		isIgnoreFolder = isIgnoreFolder == undefined ? true : isIgnoreFolder;
 		let fileList = [];
 		if(fs.statSync(folderPath).isDirectory()) {
 			if(!isIgnoreFolder) fileList.push(folderPath);
-			fs.readdirSync(folderPath).forEach(function(file) {
+			fs.readdirSync(folderPath).forEach(file => {
 				fileList = fileList.concat(this.getFileList(folderPath + '/' + file, isIgnoreFolder));
 			});
 		}
@@ -95,5 +95,26 @@ export default {
 			throw new Error('url can not be null.');
 		}
 		require('child_process').exec(`${require('os').platform() === 'win32' ? 'start' : 'open'} ${url}`);
-	}
+	},
+	/**
+	 * 统计某一个文件的行数
+	 * @param {*} filePath 文件路径
+	 */
+	countFileLine(filePath) {
+		return (fs.readFileSync(filePath).toString('utf8') || '').split('\n').length;
+	},
+	/**
+	 * 统计某个文件夹里面所有文件的总行数
+	 * @param {*} folderPath 
+	 */
+	countFolderFileLine(folderPath) {
+		let lineCount = 0;
+		this.getFileList(folderPath).forEach(filePath => {
+			const count = this.countFileLine(filePath);
+			lineCount += count;
+			console.log(`${filePath}行数：${count}`);
+		});
+		console.log(`总行数：${lineCount}`);
+		return lineCount;
+	},
 }
---
+++ test/count-file-line.js
@@ -0,0 +1,6 @@
+/**
+ * 统计某个文件夹代码行数
+ */
+const xei = require('../js/xei-node');
+// xei.countFolderFileLine('/Users/liuxianan/workspace/web/ota-po-junye/src/pages/detail');
+xei.countFolderFileLine('/Users/liuxianan/workspace/web/ota-po/src/pages/po/placeOrder');
